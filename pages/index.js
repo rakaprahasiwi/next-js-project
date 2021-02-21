a05cb705dcda2217067fdf5081381c59f9c2697c
rakaprahasiwi <prahasiwi11@gmail.com>
2021-02-22 02:16:27 +0700

    catch error in promise

--- pages/index.js
+++ pages/index.js
@@ -86,14 +86,17 @@ class Home extends React.Component {
   // }
 
   componentDidMount() {
-    getMovies().then((movies) => {
-      this.setState({ movies })
-
-    })
+    getMovies()
+      .then((movies) => {
+        this.setState({ movies })
+      })
+      .catch((error) => {
+        this.setState({ errorMessage: error })
+      })
   }
 
   render() {
-    const { movies } = this.state
+    const { movies, errorMessage } = this.state
     return (
       <div className={styles.container} >
         <Head>
@@ -118,6 +121,11 @@ class Home extends React.Component {
                 <Carousel />
 
                 <div className="row">
+                  {errorMessage &&
+                    <div className="alert alert-danger" role="alert">
+                      {errorMessage}
+                    </div>
+                  }
                   <MovieList movies={movies} />
 
                 </div>
